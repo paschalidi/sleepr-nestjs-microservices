2ba6fa480ac9da876ec553e048fae2ff24c3ceff
Christos Paschalidis <paschalidi@outlook.com>
2023-08-03 10:04:32 -0600

    adds email notifications microservice

---
+++ apps/notifications/src/notifications.controller.ts
@@ -0,0 +1,16 @@
+import {Controller, Get, UsePipes, ValidationPipe} from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
+import {EventPattern, Payload} from "@nestjs/microservices";
+import {NotifyEmailDto} from "./dto/notify-email.dto";
+
+@Controller()
+export class NotificationsController {
+  constructor(private readonly notificationsService: NotificationsService) {}
+
+  @EventPattern('notify_email')
+  @UsePipes(new ValidationPipe())
+  async notifyEmail(@Payload() {email, text}: NotifyEmailDto) {
+    await this.notificationsService.notifyEmail({email, text})
+  }
+
+}
